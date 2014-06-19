b32e23a27cb40edadefaa12ba7b00235ead16c08
liuqi <ngaut@126.com>
2014-06-19 12:01:01 +0800

    refactor

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"os"
 	"strings"
 	"time"
 
@@ -10,7 +9,7 @@ import (
 	log "github.com/ngaut/logging"
 	"github.com/ngaut/tyrant/scheduler"
 	"github.com/ngaut/tyrant/scheduler/mesosrel"
-	myzk "github.com/ngaut/tyrant/zk"
+	"github.com/ngaut/tyrant/zkhelper"
 	"github.com/samuel/go-zookeeper/zk"
 )
 
@@ -26,11 +25,10 @@ func Init() {
 }
 
 const (
-	tyrant_zk_path    = "/hosts/"
-	tyrant_tmp_master = tyrant_zk_path + "master"
+	tyrant_zk_path = "/zk/tyrant/"
 )
 
-func promote() {
+func tryRunAsLeader() {
 	config := cfg.NewCfg(*conf)
 	err := config.Load()
 	if err != nil {
@@ -39,62 +37,19 @@ func promote() {
 	zkaddr, err := config.ReadString("zk", "localhost:2181/tyrant")
 	log.Debug(zkaddr)
 
-	zkConn, err := myzk.Connect(strings.Split(zkaddr, ","), 3*time.Second)
+	zkConn, _, err := zk.Connect(strings.Split(zkaddr, ","), 3*time.Second)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	if exist, _, err := zkConn.Exists(tyrant_zk_path); err == nil && !exist {
-		err = myzk.Create(zkConn, tyrant_zk_path)
-		if err != nil && err != zk.ErrNodeExists {
-			log.Fatal(err)
-		}
-	}
-
-	hostName, _ := os.Hostname()
-	if exist, _, err := zkConn.Exists(tyrant_tmp_master); err == nil && !exist {
-		err = myzk.RegisterTemp(zkConn, tyrant_tmp_master, hostName)
-		if err != nil && err != zk.ErrNodeExists {
-			log.Fatal(err)
-		}
-
-		return
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//watch it
-	_, _, evtCh, err := zkConn.GetW(tyrant_tmp_master)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		evt := <-evtCh
-		log.Fatalf("%+v", evt)
-		switch evt.Type {
-		case zk.EventNodeDeleted:
-			err = myzk.RegisterTemp(zkConn, tyrant_tmp_master, hostName)
-			if err != nil && err != myzk.ErrNodeNotExist {
-				log.Fatal(err)
-			}
-			if err == nil {
-				return
-			}
-		default:
-			log.Fatalf("%+v", evt)
-		}
-	}
+	leader := zkhelper.CreateElection(*zkConn, tyrant_zk_path)
+	task := &LeaderTask{}
+	leader.RunTask(task)
 }
 
-func main() {
-	flag.Parse()
-	Init()
+type LeaderTask struct {
+}
 
-	promote()
-
+func (self *LeaderTask) Run() error {
 	resScheduler := mesosrel.NewResMan()
 	go func() {
 		log.Debug("master:", *master)
@@ -106,4 +61,24 @@ func main() {
 	}()
 	s := scheduler.NewServer(":9090", resScheduler)
 	s.Serve()
+
+	log.Fatal("End of Run()")
+
+	return nil
+}
+
+func (self *LeaderTask) Stop() {
+	log.Fatal("stop server")
+}
+
+func (self *LeaderTask) Interrupted() bool {
+	log.Fatal("Interrupted")
+
+	return false
+}
+
+func main() {
+	flag.Parse()
+	Init()
+	tryRunAsLeader()
 }
