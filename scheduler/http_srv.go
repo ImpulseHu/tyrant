b3ea112040a92e29b0f3fcf049448828178f4aca
Fan DANG <dangfan@wandoujia.com>
2014-07-21 13:30:28 +0800

    added ldap authentication

--- scheduler/http_srv.go
+++ scheduler/http_srv.go
@@ -2,12 +2,17 @@ package scheduler
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	_ "net/http/pprof"
+	"os"
 	"strconv"
 	"time"
 
-	"github.com/hoisie/web"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/auth"
+	"github.com/mqu/openldap"
 	log "github.com/ngaut/logging"
 )
 
@@ -34,137 +39,140 @@ func NewServer(addr string, notifier Notifier) *Server {
 	return s
 }
 
-func responseJson(ctx *web.Context, statusCode int, obj interface{}) string {
-	ctx.WriteHeader(statusCode)
+func responseJson(statusCode int, obj interface{}) (int, string) {
 	if obj != nil {
 		content, _ := json.MarshalIndent(obj, " ", "  ")
-		return string(content)
+		return statusCode, string(content)
 	}
-	return ""
+	return statusCode, ""
 }
 
-func responseError(ctx *web.Context, ret int, msg string) string {
-	return responseJson(ctx, 500, map[string]interface{}{
+func responseError(ret int, msg string) (int, string) {
+	return responseJson(500, map[string]interface{}{
 		"ret": ret,
 		"msg": msg,
 	})
 }
 
-func responseSuccess(ctx *web.Context, data interface{}) string {
-	return responseJson(ctx, 200, map[string]interface{}{
+func responseSuccess(data interface{}) (int, string) {
+	return responseJson(200, map[string]interface{}{
 		"ret":  0,
 		"data": data,
 	})
 }
 
-func jobList(ctx *web.Context) string {
+func jobList() (int, string) {
 	jobs := GetJobList()
 	if jobs != nil && len(jobs) > 0 {
-		return responseSuccess(ctx, jobs)
+		return responseSuccess(jobs)
 	}
 
-	return responseSuccess(ctx, nil)
+	return responseSuccess(nil)
 }
 
-func jobUpdate(ctx *web.Context, id string) string {
+func jobUpdate(params martini.Params, req *http.Request) (int, string) {
+	id := params["id"]
 	if JobExists(id) {
-		b, err := ioutil.ReadAll(ctx.Request.Body)
+		b, err := ioutil.ReadAll(req.Body)
 		if err != nil {
-			return responseError(ctx, -2, err.Error())
+			return responseError(-2, err.Error())
 		}
 		var job Job
 		err = json.Unmarshal(b, &job)
 		j, _ := GetJobById(id)
 		if err != nil {
-			return responseError(ctx, -3, err.Error())
+			return responseError(-3, err.Error())
 		}
 		job.Id = j.Id
 		if err := job.Save(); err != nil {
-			return responseError(ctx, -4, err.Error())
+			return responseError(-4, err.Error())
 		}
-		return responseSuccess(ctx, job)
+		return responseSuccess(job)
 	}
 
-	return responseError(ctx, -5, "no such job")
+	return responseError(-5, "no such job")
 }
 
-func jobNew(ctx *web.Context) string {
-	b, err := ioutil.ReadAll(ctx.Request.Body)
+func jobNew(req *http.Request) (int, string) {
+	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		return responseError(ctx, -1, err.Error())
+		return responseError(-1, err.Error())
 	}
 
 	var job Job
 	err = json.Unmarshal(b, &job)
 	if err != nil {
-		return responseError(ctx, -2, err.Error())
+		return responseError(-2, err.Error())
 	}
 
 	job.CreateTs = time.Now().Unix()
 	err = sharedDbMap.Insert(&job)
 	if err != nil {
-		return responseError(ctx, -3, err.Error())
+		return responseError(-3, err.Error())
 	}
 
-	return responseSuccess(ctx, job)
+	return responseSuccess(job)
 }
 
-func jobRemove(ctx *web.Context, id string) string {
+func jobRemove(params martini.Params) (int, string) {
+	id := params["id"]
 	log.Debug("on job remove")
 	j, _ := GetJobById(id)
 	if j != nil {
 		if err := j.Remove(); err != nil {
-			return responseError(ctx, -2, err.Error())
+			return responseError(-2, err.Error())
 		}
-		return responseSuccess(ctx, j)
+		return responseSuccess(j)
 	}
 
-	return responseError(ctx, -3, "no such job")
+	return responseError(-3, "no such job")
 }
 
-func jobGet(ctx *web.Context, id string) string {
+func jobGet(params martini.Params) (int, string) {
+	id := params["id"]
 	j, err := GetJobById(id)
 	if err != nil {
-		return responseError(ctx, -1, err.Error())
+		return responseError(-1, err.Error())
 	}
 
-	return responseSuccess(ctx, j)
+	return responseSuccess(j)
 }
 
-func jobRun(ctx *web.Context, id string) string {
+func jobRun(params martini.Params) (int, string) {
+	id := params["id"]
 	j, err := GetJobById(id)
 	if err != nil {
-		return responseError(ctx, -1, err.Error())
+		return responseError(-1, err.Error())
 	}
 
 	if s.notifier != nil && j != nil {
 		taskId, err := s.notifier.OnRunJob(j)
 		if err != nil {
 			log.Debug(err.Error())
-			return responseError(ctx, -2, err.Error())
+			return responseError(-2, err.Error())
 		}
 
-		return responseSuccess(ctx, taskId)
+		return responseSuccess(taskId)
 	}
 
 	log.Debug("Notifier not found")
-	return responseError(ctx, -3, "notifier not found")
+	return responseError(-3, "notifier not found")
 }
 
-func jobRunOnce(ctx *web.Context) string {
-	b, err := ioutil.ReadAll(ctx.Request.Body)
+func jobRunOnce(req *http.Request) (int, string) {
+	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		return responseError(ctx, -1, err.Error())
+		return responseError(-1, err.Error())
 	}
 	var j Job
 	err = json.Unmarshal(b, &j)
 	if err != nil {
-		return responseError(ctx, -2, err.Error())
+		return responseError(-2, err.Error())
 	}
 
 	jobTpl, err := GetJobById(strconv.Itoa(int(j.Id)))
 	if err != nil {
-		return responseError(ctx, -1, err.Error())
+		return responseError(-1, err.Error())
 	}
 
 	j.Owner = jobTpl.Owner
@@ -178,26 +186,27 @@ func jobRunOnce(ctx *web.Context) string {
 		taskId, err := s.notifier.OnRunJob(&j)
 		if err != nil {
 			log.Debug(err.Error())
-			return responseError(ctx, -2, err.Error())
+			return responseError(-2, err.Error())
 		}
 
-		return responseSuccess(ctx, taskId)
+		return responseSuccess(taskId)
 	}
 
 	log.Debug("Notifier not found")
-	return responseError(ctx, -3, "notifier not found")
+	return responseError(-3, "notifier not found")
 }
 
-func taskList(ctx *web.Context) string {
+func taskList() (int, string) {
 	tasks := GetTaskList()
 	if tasks != nil && len(tasks) > 0 {
-		return responseSuccess(ctx, tasks)
+		return responseSuccess(tasks)
 	}
 
-	return responseSuccess(ctx, nil)
+	return responseSuccess(nil)
 }
 
-func taskKill(ctx *web.Context, id string) string {
+func taskKill(params martini.Params) string {
+	id := params["id"]
 	if s.notifier != nil {
 		err := s.notifier.OnKillTask(id)
 		if err != nil {
@@ -209,17 +218,37 @@ func taskKill(ctx *web.Context, id string) string {
 	return "error:notifier not registered"
 }
 
-func (srv *Server) Serve() {
-	web.Get("/job/list", jobList)
-	web.Get("/task/list", taskList)
-	web.Get("/task/kill/(.*)", taskKill)
-	web.Get("/job/(.*)", jobGet)
-	web.Post("/job", jobNew)
-	web.Post("/job/runonce", jobRunOnce)
-	web.Post("/job/run/(.*)", jobRun)
-	web.Delete("/job/(.*)", jobRemove)
-	web.Put("/job/(.*)", jobUpdate)
+func authenticate(username, password string) bool {
+	ldap_server, _ := globalCfg.ReadString("ldap_server", "")
+	dn_fmt, _ := globalCfg.ReadString("dn_fmt", "")
+	ldap, err := openldap.Initialize(ldap_server)
+	if err != nil {
+		panic(err)
+	}
+	ldap.SetOption(openldap.LDAP_OPT_PROTOCOL_VERSION, openldap.LDAP_VERSION3)
+	dn := fmt.Sprintf(dn_fmt, username)
+	err = ldap.Bind(dn, password)
+	ldap.Close()
+	return err == nil
+}
 
-	addr, _ := globalCfg.ReadString("http_addr", ":9090")
-	web.Run(addr)
+func (srv *Server) Serve() {
+	m := martini.Classic()
+
+	m.Use(martini.Static("static"))
+	m.Use(auth.BasicFunc(authenticate))
+
+	m.Get("/job/list", jobList)
+	m.Get("/task/list", taskList)
+	m.Get("/task/kill/:id", taskKill)
+	m.Get("/job/:id", jobGet)
+	m.Post("/job", jobNew)
+	m.Post("/job/runonce", jobRunOnce)
+	m.Post("/job/run/:id", jobRun)
+	m.Delete("/job/:id", jobRemove)
+	m.Put("/job/:id", jobUpdate)
+
+	addr, _ := globalCfg.ReadString("http_addr", "9090")
+	os.Setenv("PORT", addr)
+	m.Run()
 }
