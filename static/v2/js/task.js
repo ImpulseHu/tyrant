76d4d67063d0e8f4fda17200f50bf09db8a30fe2
dongxu <huang@wandoujia.com>
2014-07-29 14:54:40 +0800

    MOD add label color

--- static/v2/js/task.js
+++ static/v2/js/task.js
@@ -1,2 +1,17 @@
 $(document).ready(function(){
+
+  $('.kill-btn').click(function() {
+    var task_id = $(this).data('id');
+    $.ajax({
+      url : '/task/kill/' + task_id,
+      type: 'GET',
+      success: function(e) {
+        alert(e);
+      },
+      error: function(xhr, text, err) {
+        alert(xhr.responseText);
+      }
+    });
+  });
+
 });
